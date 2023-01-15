38eb7411ce65d8ad08ca8a2167704dde0070927a
Nicolas PENCRÉACH <nicolas.pencreach@son-video.com>
2023-01-15 10:20:15 +0100

    create some colors

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,12 @@
 export const WIDTH = 155
 export const HEIGHT = 201
 export const YEAR = 2023
+
+export const colors = {
+    white: 255,
+    black: 0,
+    'bg-gray-dark': 80,
+    'bg-gray': 140,
+    'bg-gray-light': 230,
+    'text-on-gray-dark': 240,
+}
--- src/doc.js
+++ src/doc.js
@@ -1,5 +1,5 @@
 import { jsPDF } from 'jspdf'
-import { HEIGHT, WIDTH, YEAR } from './constants.js'
+import { colors, HEIGHT, WIDTH, YEAR } from './constants.js'
 import { addSketchyTimesFontToDoc } from './font/SketchyTimes-normal.js'
 import { addSketchyTimesBoldFontToDoc } from './font/SketchyTimes-bold.js'
 
@@ -19,16 +19,16 @@ addSketchyTimesBoldFontToDoc(doc)
 // =====
 
 // background
-doc.setFillColor(80)
+doc.setFillColor(colors['bg-gray-dark'])
 doc.rect(0, 0, WIDTH, HEIGHT, 'F')
 
 // text on cover
-doc.setFont('SketchyTimes', 'bold')
 const font_size = 96
 const line_height = 30
-doc.setFontSize(font_size)
-doc.setTextColor(240)
 let y = 0
+doc.setFont('SketchyTimes', 'bold')
+doc.setFontSize(font_size)
+doc.setTextColor(colors['text-on-gray-dark'])
 doc.text('Agenda', 32, (y += 60))
 doc.text('&Notes', 32, (y += line_height))
 doc.text(`${YEAR}`, 50, (y += 1.5 * line_height))
