6bbf2af956cd2d31d10ab9567e0d20b7a7dd2e7c
Nicolas PENCRÉACH <nicolas.pencreach@son-video.com>
2023-01-15 16:04:29 +0100

    change number of days dynamically + clean main.js

--- main.js
+++ main.js
@@ -6,41 +6,17 @@ import { PAGE_TYPE_DAY } from './src/page/day.js'
 import { PAGE_TYPE_NOTES_INDEX } from './src/page/notes_index.js'
 import { PAGE_TYPE_NOTE } from './src/page/note.js'
 import { generatePages } from './src/generator.js'
+import { getDaysInYear } from 'date-fns'
+import { YEAR } from './src/constants.js'
 
 const DESTINATION = 'build/daily_organizer.pdf'
-
-// doc.textWithLink("Click me!", 10, 10, {
-//     url: "https://parall.ax/"
-// });
-// doc.createAnnotation({
-//     type: "text",
-//     title: "note",
-//     bounds: {
-//         x: 10,
-//         y: 10,
-//         w: 200,
-//         h: 80
-//     },
-//     contents: "This is text annotation (closed by default)",
-//     open: false
-// });
-// doc.textWithLink('Page 2', 10, 100, {
-//     pageNumber: 2,
-// })
-// doc.textWithLink('Page 3', 10, 120, {
-//     pageNumber: 3,
-// })
-// doc.link(50, 50, 20, 20, { pageNumber: 3 })
-// doc.text('Test', 50, 60)
-// doc.addPage()
-// doc.addPage()
 
 // create structure
 pager.addPages(PAGE_TYPE_YEAR, 1)
 pager.addPages(PAGE_TYPE_QUARTER, 4)
-pager.addPages(PAGE_TYPE_DAY, 365)
+pager.addPages(PAGE_TYPE_DAY, getDaysInYear(new Date(YEAR, 0, 1)))
 pager.addPages(PAGE_TYPE_NOTES_INDEX, 1)
-pager.addPages(PAGE_TYPE_NOTE, 10)
+pager.addPages(PAGE_TYPE_NOTE, 200)
 
 generatePages()
 
