11a57a2d8b0e132e093a39f9bf5122db84f7973b
Nicolas PENCRÉACH <nicolas.pencreach@son-video.com>
2023-01-18 00:45:55 +0100

    refactor translation mechanism to use templates

--- src/translation.js
+++ src/translation.js
@@ -7,83 +7,40 @@ import { YEAR } from './constants.js'
 const DEFAULT_LOCALE = 'fr'
 
 // Please add more translations and keep "fr" key
-const TRANSLATIONS = {
+const TRANSLATORS = {
     fr: {
-        cover_line1: 'Agenda',
-        cover_line2: '&Notes',
-        calendar_title: `Calendrier ${YEAR}`,
-        notes_tab: 'Notes',
-        day_bloc_title_1: 'Top priorités',
-        day_bloc_title_2: 'Autres / Futur',
-        day_bloc_title_3: 'Notes du jour',
+        calendar_title: () => `Calendrier ${YEAR}`,
+        cover_line1: () => 'Agenda',
+        cover_line2: () => '&Notes',
+        day_bloc_title_1: () => 'Top priorités',
+        day_bloc_title_2: () => 'Autres / Futur',
+        day_bloc_title_3: () => 'Notes du jour',
+        day_letter: (date) => format(date, 'EEEEE', { locale: fr }), // First letter of day
+        day_long: (date) => format(date, 'iii dd LLLL', { locale: fr }).split(' ').map(_.upperFirst).join(' '), // Day full name
+        day_short: (date) => format(date, 'dd EEEEE', { locale: fr }), // Number + First letter
+        month_label: (date) => _.upperFirst(format(date, 'MMMM', { locale: fr })), // Month in plain text
+        notes_tab: () => 'Notes',
+        quarter_short_label: (quarter_number) => `T${quarter_number}`,
     },
 }
 
 let locale = DEFAULT_LOCALE
 const setLocale = (l) => (locale = l)
 
-const getLongFormattedDate = (date) => {
-    let trans = 'getLongFormattedDate trans'
-    switch (locale) {
-        case 'fr':
-            trans = format(date, 'iii dd LLLL', { locale: fr }).split(' ').map(_.upperFirst).join(' ')
-            break
+const getTranslator = (keyword) => {
+    if (TRANSLATORS?.[locale]?.[keyword]) {
+        return TRANSLATORS[locale][keyword]
     }
 
-    return trans
+    return () => keyword
 }
 
-const getFor = (keyword) => {
-    if (TRANSLATIONS?.[locale]?.[keyword]) {
-        return TRANSLATIONS[locale][keyword]
-    }
-
-    return keyword
-}
-
-const getQuarterShortLabel = (quarter_number) => {
-    let trans = 'getDayShort trans'
-    switch (locale) {
-        case 'fr':
-            trans = `T${quarter_number}`
-            break
-    }
-
-    return trans
-}
-
-const getMonthLabel = (date) => {
-    let trans = 'getMonthLabel trans'
-    switch (locale) {
-        case 'fr':
-            trans = _.upperFirst(format(date, 'MMMM', { locale: fr }))
-            break
-    }
-
-    return trans
-}
-
-const getDayShort = (date) => {
-    let trans = 'getDayShort trans'
-    switch (locale) {
-        case 'fr':
-            trans = format(date, 'dd EEEEE', { locale: fr })
-            break
-    }
-
-    return trans
-}
-
-const getDayLetter = (date) => {
-    let trans = 'getDayShort trans'
-    switch (locale) {
-        case 'fr':
-            trans = format(date, 'EEEEE', { locale: fr })
-            break
-    }
-
-    return trans
-}
+const getFor = (keyword) => getTranslator(keyword)()
+const getQuarterShortLabel = (quarter_number) => getTranslator('quarter_short_label')(quarter_number)
+const getMonthLabel = (date) => getTranslator('month_label')(date)
+const getLongFormattedDate = (date) => getTranslator('day_long')(date)
+const getDayShort = (date) => getTranslator('day_short')(date)
+const getDayLetter = (date) => getTranslator('day_letter')(date)
 
 export default {
     setLocale,
